c99ebce3e999cd2bd358aadcf067c853cb911379
JingTingZeng <neux.jingtingzeng@gmail.com>
2020-07-19 19:26:51 +0800

    串接聯絡人資訊

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Contact } from './model/contact';
+declare const device;
+declare const navigator;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
   title = 'contact-app';
 
-  public contactList: { name: string, phone: string, avatar: string }[] = [];
+  public contactList: Array<Contact>;
 
-  ngOnInit(): void {
+  constructor(private zone: NgZone) { }
+
+  ngOnInit() {
+    document.addEventListener('deviceready', () => {
+      this.getContacts();
+    }, false);
     this.contactList = [
-      { name: 'jing ting', phone: '0977886677', avatar: '' },
-      { name: 'jing ting', phone: '0977886677', avatar: '' },
-      { name: 'test', phone: '0977886677', avatar: '' }
+      new Contact('jing ting', '0988778899', ''),
+      new Contact('jing ting', '0977886677', ''),
+      new Contact('test', '0977886677', '')
     ];
+  }
+
+  // onDeviceReady() {
+  //   let device;
+  //   alert(device.cordova);
+  // }
+
+  getContacts() {
+    navigator.contacts.find(['name', 'phoneNumbers', 'photos'], this.contactsSuccess.bind(this), this.contactsFail.bind(this));
+  }
+
+  contactsSuccess(contacts) {
+    const addItems = [];
+    // alert('Found ' + JSON.stringify(contacts) + ' contacts.');
+    contacts.forEach((item) => {
+      const newItem = new Contact(item.name.formatted, item.phoneNumbers[0].value, item.photos);
+      addItems.push(newItem);
+    });
+    this.zone.run(() => {
+      this.contactList = [...this.contactList, ...addItems];
+    });
+  }
+
+  contactsFail(msg) {
+    alert('onError: ' + msg);
   }
 }
